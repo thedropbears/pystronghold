f8e809a5e74cf7bd19b302123db8bd99e563b0af
Arthur Allshire <arthur.allshire@gmail.com>
2016-01-19 08:54:19 +1100

    Initial yield system implementation

--- robot.py
+++ robot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import wpilib
+import time
 from wpilib import command
 
 #from subsystems.example_subsystem import ExampleSubsystem
@@ -9,6 +10,20 @@ from subsystems import Chassis
 from oi import OI
 
 import logging
+
+def omni_drive(robot):
+    while robot.omni_driving:
+        robot.chassis.drive(robot.oi.getJoystickY(), robot.oi.getJoystickX(), robot.oi.getJoystickZ(), robot.oi.getThrottle())
+        yield
+
+def log_message(robot):
+    time = time.time()
+    robot.logger.info("log message run")
+    print("logged")
+    #only run every 3 seconds
+    while time.time()-time < 3.0:
+        yield
+
 
 class StrongholdRobot(wpilib.IterativeRobot):
 
@@ -19,34 +34,75 @@ class StrongholdRobot(wpilib.IterativeRobot):
         This function is called upon program startup and
         should be used for any initialization code.
         """
+        self.running = {}
+        self.omni_driving = True
+        self.omni_drive = omni_drive
         self.oi = OI(self)
         self.chassis = Chassis(self)
         self.logger = logging.getLogger("robotpy")
         #Create the command used for the autonomous period
         #self.autonomous_command = ExampleCommand(self)
+        self.taskmap = {11:log_message}
+        self.auto_tasks = [] # [[list, of, tasks, to_go, through, sequentially], [and, this, list, will, run, in, parallel]
+        self.current_auto_tasks = []
 
     def disabledInit(self):
         pass
 
     def disabledPeriodic(self):
         """This function is called periodically when disabled."""
-        command.Scheduler.getInstance().run()
+        self.running = {}
 
     def autonomousInit(self):
         #Schedule the autonomous command
         #self.autonomous_command.start()
-        pass
+        self.running = {}
+        self.current_auto_tasks = self.auto_tasks
 
     def autonomousPeriodic(self):
         """This function is called periodically during autonomous."""
-        command.Scheduler.getInstance().run()
+        # clear empty task sequences
+        for i in range(len(self.auto_tasks)):
+            if len(self.auto_tasks[i]) == 0:
+                self.auto_tasks[i].pop()
+        for command_sequence in self.auto_tasks:
+            if command_sequence[0] not in self.running:
+                ifunc = command_sequence[0](self).__next__
+                self.running[command_sequence[0]] = ifunc
+        done = []
+        for key, ifunc in self.running.items():
+            try:
+                ifunc(self)
+            except StopIteration:
+                done.append(key)
+        for key in done:
+            self.running.pop(key)
+            # remove the done task from our list of auto commands
+            for command_sequence in self.running:
+                if command_sequence[0] == done:
+                    command_seqeuence[0].pop()
 
     def teleopInit(self):
-        pass
+        self.running = {}
 
     def teleopPeriodic(self):
         """This function is called periodically during operator control."""
-        command.Scheduler.getInstance().run()
+        for button, task in self.taskmap.items():
+            #if the button for the task is pressed and the task is not already running
+            if self.oi.joystick.getRawButton(button) and task not in self.running:
+                ifunc = task(self).__next__
+                self.running[task] = ifunc
+        if self.omni_driving and self.omni_drive not in self.running:
+            ifunc = self.omni_drive(self).__next__
+            self.running[self.omni_drive] = ifunc
+        done = []
+        for key, ifunc in self.running.items():
+            try:
+                ifunc()
+            except StopIteration:
+                done.append(key)
+        for key in done:
+            self.running.pop(key)
 
     def testPeriodic(self):
         """This function is called periodically during test mode."""
--- tests/test_yield_system.py
+++ tests/test_yield_system.py
@@ -7,34 +7,43 @@ def test_disabled(robot, control, fake_time):
     robot.running = {}
     # run disabled for 15 seconds
     control.set_autonomous(enabled=False)
+    robot.disabledInit()
+    for i in range(10):
+        robot.disabledPeriodic()
     # run for 15 fake seconds
-    control.run_test(lambda tm: tm < 15)
-    assert len(robot.runnning) == 0
+    #control.run_test(lambda tm: tm < 15)
+    assert len(robot.running) == 0
     # ensure we ran for 15 seconds of fake time
-    assert int(fake_time.get()) == 15
+    #assert int(fake_time.get()) == 15
 
-def test_teleop(robot, control, fake_time):
+def test_teleop(robot, control, fake_time, hal_data):
     robot.robotInit()
 
     # test that omni drive does run when its flag is true
     robot.running = {}
-    control.omni_driving= False
+    robot.omni_driving= False
     control.set_operator_control(enabled = True)
+    robot.teleopInit()
+    for i in range(10):
+        hal_data["joysticks"][0]["buttons"][11] = True
+        robot.teleopPeriodic()
     # run for 15 fake seconds
-    control.run_test(lambda tm: tm < 15)
-    assert omni_drive in robot.running
+    #control.run_test(lambda tm: tm < 15)
+    assert len(robot.running) == 0
     # ensure we ran for 15 seconds of fake time
-    assert int(fake_time.get()) == 15
+    #assert int(fake_time.get()) == 15
 
     # test that omni drive does not run when its flag is false
     robot.running = {}
-    control.omni_driving= False
+    robot.omni_driving= True
     control.set_operator_control(enabled = True)
+    for i in range(10):
+        robot.teleopPeriodic()
     # run for 15 fake seconds
-    control.run_test(lambda tm: tm < 15)
-    assert not omni_drive in robot.running
+    #control.run_test(lambda tm: tm < 15)
+    assert len(robot.running) == 1
     # ensure we ran for 15 seconds of fake time
-    assert int(fake_time.get()) == 15
+    #assert int(fake_time.get()) == 15
 
 def test_omni_drive(robot, control, fake_time, hal_data):
     robot.robotInit()
@@ -43,15 +52,17 @@ def test_omni_drive(robot, control, fake_time, hal_data):
         module._direction = 0.0
         module._speed = 0.0
     robot.running = {}
-    control.omni_driving = True
+    robot.omni_driving = True
     control.set_operator_control(enabled = True)
     # robot's x axis to 1.0
     hal_data['joysticks'][0][1] = -1.0
     # robot's throttle to 1.0
     hal_data['joysticks'][0][3] = -1.0
-    control.run_test(lambda tm: tm < 15)
+    robot.teleopInit()
+    for i in range(10):
+        robot.teleopPeriodic()
+    #control.run_test(lambda tm: tm < 15)
     # ensure that the robot is going forwards
     for module in robot.chassis._modules:
-        assert (module.speed - 1.0) < epsilon
-    assert omni_drive in robot.running
-    assert int(fake_time.get()) - 15
+        assert (module._speed - 1.0) < epsilon
+    #assert int(fake_time.get()) - 15
