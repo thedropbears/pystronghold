1f042d2bf6867c155e017699c7d1518dde9cd45d
James Ward <james@thedropbears.org.au>
2016-02-09 14:47:04 +1100

    Tests for intake. Use Talon's method to get current.

--- components/intake.py
+++ components/intake.py
@@ -4,6 +4,7 @@ from wpilib import CANTalon, PowerDistributionPanel
 from components import shooter
 
 import logging
+from _collections import deque
 
 class States:
     no_ball = 0
@@ -17,71 +18,50 @@ class Intake:
     intake_motor = CANTalon
     shooter = shooter.Shooter
 
-    pdp_channel = 15
-
     def __init__(self):
         self._speed = 0.0
         self.state = States.no_ball
-        self.pdp = PowerDistributionPanel()
-        self.current_counter = 0.0
-        self.spool_up = 0.0
-        self.changed_state = True
-
-    def change_state(self, state):
-        if state != self.state:
-            self.state = state
-            self.changed_state = True
+        self.current_deque = deque([0.0] * 5, 5)  # Used to average currents over last n readings
 
     def toggle(self):
         if self.state == States.intaking_free:
             self.state = States.no_ball
         else:
             self.state = States.intaking_free
-        self.changed_state = True
 
     def execute(self):
-        current = self.pdp.getCurrent(self.pdp_channel)
-        if current != 0.0:
-            logging.getLogger("intake").info(self.pdp.getCurrent(self.pdp_channel))
-            logging.getLogger("intake").info("State: " + str(self.state))
+        # add next reading on right, will automatically pop on left
+        self.current_deque.append(self.intake_motor.getOutputCurrent())
+        maxlen = self.current_deque.maxlen
+        current_avg = sum(self.current_deque) / maxlen
+        current_rate = self.current_deque[maxlen-1]-self.current_deque[maxlen-2]
+
         if self.state == States.no_ball:
             self._speed = 0.0
+
         if self.state == States.intaking_free:
+            self.intake_motor.setVoltageRampRate(6.0)  # V/s
             self._speed = 1.0
             self.shooter.change_state(shooter.States.backdriving)
-            self.spool_up += 1
-            if current > 3.0 and self.spool_up > 20: # amps
-                self.current_counter += 1
-                if self.current_counter >= 3:
-                    self.change_state(States.intaking_contact)
-                    self.current_counter = 0
-                    self.spool_up = 0
-            elif current != 0:
-                self.current_counter = 0
+            if current_avg > 3.0 and current_rate >= 0.0:  # amps
+                self.state = States.intaking_contact
+
         if self.state == States.intaking_contact:
             self._speed = 1.0
-            self.shooter.change_state(shooter.States.backdriving)
-            if current < 3.0: # amps
-                self.current_counter += 1
-                if self.current_counter >= 2:
-                    self.change_state(States.pinning)
-                    self.current_counter = 0
-            elif current != 0:
-                self.current_counter = 0
+            if current_avg < 3.0 and current_rate <= 0.0:  # amps
+                self.state = States.pinning
+
         if self.state == States.pinning:
             self._speed = -0.4
-            self.shooter.change_state(shooter.states.off)
-            if current > 5.0:
-                self.current_counter += 1
-                if self.current_counter >= 2:
-                    self.change_state(States.pinned)
-                    self.current_counter = 0
-            elif current != 0:
-                self.current_counter = 0
+            if current_avg > 5.0 and current_rate >= 0.0:
+                self.state = States.pinned
+
         if self.state == States.pinned:
+            self.shooter.change_state(shooter.States.off)
             self._speed = 0.0
+
         if self.state == States.fire:
+            self.intake_motor.setVoltageRampRate(99999.0)  # Max ramp rate
             self._speed = 1.0
-        if self.changed_state:
-            self.intake_motor.set(-self._speed)
-            self.changed_state = False
+
+        self.intake_motor.set(-self._speed)
--- tests/test_intake.py
+++ tests/test_intake.py
@@ -1,19 +1,53 @@
 from components.intake import Intake, States
 from unittest.mock import MagicMock
 
-def test_toggle():
+
+
+def test_state_transition():
     intake = Intake()
     intake.intake_motor = MagicMock()
     intake.shooter = MagicMock()
-    intake.changed_state = False
-    intake.states = States.no_ball
-    intake.toggle()
+    intake.intake_motor.getOutputCurrent = MagicMock(side_effect=
+                                                     [0,  # no ball
+                                                      2, 2, 2, 2, 2,  # intaking
+                                                      3, 4, 5, 4, 3,  # ball in
+                                                      2, 2, 2, 2, 2,  # backdriving
+                                                      2, 3, 3, 4, 6,  # pinning
+                                                      6, 6, 7, 7, 9,  # still pinning!
+                                                      4, 4, 4, 4, 4,  # FIRE!
+                                                      ])
     intake.execute()
+    assert intake.state == States.no_ball
+
+    intake.state = States.intaking_free
+    for _ in range(5):
+        intake.execute()
+    assert intake.shooter.change_state.called
+    assert intake.intake_motor.setVoltageRampRate.called
+    intake.intake_motor.setVoltageRampRate.called = False
+    assert intake.state == States.intaking_free
+
+    for _ in range(5):
+        intake.execute()
+    assert intake.state == States.intaking_contact
+
+    for _ in range(5):
+        intake.execute()
+    assert intake.state == States.pinning
+
+    for _ in range(10):
+        intake.execute()
+    assert intake.state == States.pinned
+    assert intake.shooter.change_state.called
+
+    intake.state = States.fire
     intake.execute()
     assert intake._speed != 0.0
-    intake.states = States.intaking_free
+    assert intake.intake_motor.setVoltageRampRate.called
+
+def test_toggle():
+    intake = Intake()
     intake.toggle()
-    intake.execute()
-    intake.execute()
-    assert intake._speed == 0.0
-    assert intake.intake_motor.set.call_count == 2
+    assert intake.state == States.intaking_free
+    intake.toggle()
+    assert intake.state == States.no_ball
