c84ccf8f3a892623466c7f0fa216f65bb32d36da
ArthurAllshire <arthur.allshire@gmail.com>
2016-02-16 18:26:14 +1100

    Improve range finder coverage, fix syntax and add angle trig

--- components/range_finder.py
+++ components/range_finder.py
@@ -1,14 +1,18 @@
 import wpilib
 
+import math
+
 class RangeFinder:
 
     range_finder_counter = wpilib.Counter
 
+    angle_inclination = 10.0 # deg
+
     def getDistance(self):
-        return self.range_finder_counter.getPeriod() * 1000000 / 1000  # 10 usec is 1cm, return as metres
+        return self.range_finder_counter.getPeriod() * 1000000 / 1000 * math.tan(math.degrees(self.angle_inclination))  # 10 usec is 1cm, return as metres
 
     def pidGet(self):
-        return getDistance(self)
+        return self.getDistance()
 
     def execute(self):
         pass
---
+++ tests/test_range_finder.py
@@ -0,0 +1,14 @@
+
+import pytest
+
+from unittest.mock import MagicMock
+
+from components.range_finder import RangeFinder
+
+def test_range_finder_syntax():
+    """ Super basic tests that do nothing but check for syntactical errors"""
+    rf = RangeFinder()
+    rf.range_finder_counter = MagicMock()
+    pid_value = rf.pidGet()
+    dist = rf.getDistance()
+    rf.execute()
