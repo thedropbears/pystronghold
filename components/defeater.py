ce88cea92137f473850afc48775bbfe24190ce0a
ArthurAllshire <arthur.allshire@gmail.com>
2016-02-19 19:01:14 +1100

    Fix speed and direction

--- components/defeater.py
+++ components/defeater.py
@@ -6,15 +6,15 @@ class Defeater:
     defeater_motor = CANTalon
 
     def up(self):
-        self.defeater_motor.set(1.0)
+        self.defeater_motor.set(-0.1)
 
     def down(self):
-        self.defeater_motor.set(-1.0)
+        self.defeater_motor.set(0.1)
 
     def execute(self):
         current = self.defeater_motor.getOutputCurrent()
 
-        if current > 30.0:
+        if current > 3.0:
             self.defeater_motor.set(0.0)
 
 
