84dce9e0b2d943f0d2f526454b274db537620cbc
James Ward <james@thedropbears.org.au>
2016-02-22 11:13:43 +1100

    Generate on_iteration methods in autonomous classes.

--- autonomous/low_bar_shoot.py
+++ autonomous/low_bar_shoot.py
@@ -14,21 +14,12 @@ class States:
     strafing = 2
     goal_tracking = 3
     firing = 4
+    spinning = 5
 
 class LowBarTower:
-    LEFT = 1
-    CENTRE = 2
-    RIGHT = 3
-
     chassis = Chassis
     shooter = shooter.Shooter
     intake = intake.Intake
-
-    #strafe_vx = 2.4
-    #strafe_vy = 3.8
-    strafe_vx = 1.2
-    strafe_vy = 1.0
-    strafe_distance = math.sqrt(strafe_vx**2 + strafe_vy**2)
 
     @property
     def distance(self):
@@ -48,6 +39,7 @@ class LowBarTower:
         self.chassis.set_heading_setpoint(self.chassis.bno055.getAngle())
         self.chassis.heading_hold = True
         self.chassis.autonomous = True
+        self.chassis.field_oriented = True
         self.state = States.through_low_bar
         self.shooter.change_state(shooter.States.off)
         self.intake.stop()
@@ -62,54 +54,70 @@ class LowBarTower:
         self.shooter.change_state(shooter.States.off)
         self.intake.state = intake.States.no_ball
 
-    def on_iteration(self, tm):
-        self.sd.putDouble('auto_state', self.state)
-        if self.state == States.through_low_bar:
-            self.chassis.inputs[:] = (1.0, 0.0, 0.0, 0.5)
-            if self.distance > 3.4:
-                self.zero_encoders()
-                self.state = States.strafing
-        if self.state == States.strafing:
-            self.chassis.inputs[:] = (self.strafe_vx, -self.strafe_vy, 0.0, 0.4)
-            if self.distance > self.strafe_distance:
-                self.state = States.goal_tracking
-                self.chassis.inputs[:] = (0.0, 0.0, 0.0, 1.0)
-                self.chassis.vision_pid.reset()
-        if self.state == States.goal_tracking:
-            # TODO - the trig to track the middle of the goal
-            self.shooter.change_state(shooter.States.shooting)
-            self.chassis.range_setpoint = 1.4 #m
-            self.chassis.track_vision = True
-            if self.chassis.vision_pid.onTarget():
-                self.vision_counts += 1
-            else:
-                self.vision_counts = 0
-            if self.chassis.range_pid.onTarget() and self.vision_counts >= 5:
-                self.state = States.firing
-                self.chassis.range_setpoint = 0.0
-                self.chassis.track_vision = False
-        if self.state == States.firing:
-            if self.shooter.state == shooter.States.shooting:
-                self.intake.state = intake.States.fire
-                self.chassis.field_oriented = True
+    def _generate_on_iteration(self, delta_x, delta_y, heading=0.0):
+        '''Drive forward the same amount, then move by delta_x and delta_y
+        to the position where the vision and range finder take over.
+        Final heading is specified too.'''
+        strafe_distance = (delta_x ** 2 + delta_y ** 2) ** 0.5
+        # Rescale velocity components to get a combined magnitude of 1
+        vx = delta_x / strafe_distance
+        vy = delta_y / strafe_distance
+        def on_iteration(tm):
+            self.sd.putDouble('auto_state', self.state)
+            if self.state == States.through_low_bar:
+                self.chassis.inputs[:] = (1.0, 0.0, 0.0, 0.5)
+                if self.distance > 3.4:  # This is always the same
+                    self.chassis.heading_hold_pid.set(heading)
+                    self.state = States.spinning
+            if self.state == States.spinning:
+                if self.chassis.heading_hold_pid.onTarget():
+                    self.zero_encoders()
+                    self.state = States.strafing
+            if self.state == States.strafing:
+                self.chassis.inputs[:] = (vx, vy, 0.0, 0.4)
+                if self.distance > strafe_distance:
+                    self.state = States.goal_tracking
+                    self.chassis.inputs[:] = (0.0, 0.0, 0.0, 1.0)
+                    self.chassis.vision_pid.reset()
+            if self.state == States.goal_tracking:
+                self.shooter.change_state(shooter.States.shooting)
+                self.chassis.range_setpoint = 1.4  # m
+                self.chassis.track_vision = True
+                if self.chassis.vision_pid.onTarget():
+                    self.vision_counts += 1
+                else:
+                    self.vision_counts = 0
+                if self.chassis.range_pid.onTarget() and self.vision_counts >= 5:
+                    self.state = States.firing
+                    self.chassis.range_setpoint = 0.0
+                    self.chassis.track_vision = False
+            if self.state == States.firing:
+                if self.shooter.state == shooter.States.shooting:
+                    self.intake.state = intake.States.fire
+                    self.chassis.field_oriented = True
+        return on_iteration
 
 class LowBarCentreTower(LowBarTower):
     MODE_NAME = "Low bar, CENTRE tower"
     DEFAULT = True
 
-    target = LowBarTower.CENTRE
+    def __init__(self):
+        # Barker field: delta_x = 2.4, delta_y = -3.8
+        self.on_iteration = self._generate_on_iteration(1.2, -1.0, 0.0)
 
 
 class LowBarLeftTower(LowBarTower):
     MODE_NAME = "Low bar, LEFT tower"
     DEFAULT = False
 
-    target = LowBarTower.LEFT
+    def __init__(self):
+        self.on_iteration = self._generate_on_iteration(1.4, -0.5, -math.pi / 6.0)
 
 
 class LowBarRightTower(LowBarTower):
     MODE_NAME = "Low bar, RIGHT tower"
     DEFAULT = False
 
-    target = LowBarTower.RIGHT
+    def __init__(self):
+        self.on_iteration = self._generate_on_iteration(1.4, -1.5, math.pi / 6.0)
 
