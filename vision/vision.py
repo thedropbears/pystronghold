d2bf76efed6f09bb97069aa182776502ea5a5120
James Ward <james@thedropbears.org.au>
2016-05-14 17:02:16 +1000

    Create separate file with vision processing.
    To be used as an mjpg-streamer plugin. Will do basic parsing of mjpg-streamer config file.

---
+++ vision/vision.py
@@ -0,0 +1,140 @@
+import cv2
+import numpy as np
+import argparse
+import os
+import re
+
+def findTarget(image):
+    height = image.shape[0]
+    width = image.shape[1]
+    # Convert from BGR colourspace to HSV. Makes thresholding easier.
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    # #Define the colours to look for (in HSV)
+    lower_colour = np.array([80 * 0.5, 0.7 * 255, 0.08 * 255])
+    upper_colour = np.array([220 * 0.5, 1.0 * 255, 0.63 * 255])
+    # Create a mask that filters out only those colours
+    mask = cv2.inRange(hsv_image, lower_colour, upper_colour)
+    # Errode and dialate the image to get rid of noise
+    kernel = np.ones((4, 4), np.uint8)
+    erosion = cv2.erode(mask, kernel, iterations=1)
+    dilated = cv2.dilate(erosion, kernel, iterations=1)
+    # Get the information for the contours
+    _, contours, __ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # sort the contours into a list
+    areas = [cv2.contourArea(contour) for contour in contours]
+    # and retrieve the largest contour in the list
+    cnt = None
+    try:
+        cnt = contours[np.argmax(areas)]
+    except ValueError:
+        return dilated
+
+    # Draw the contours
+    cv2.drawContours(image, contours, 0, (255, 0, 0), 1)
+
+    # get the area of the contour
+    area = cv2.contourArea(cnt)
+    if area / width / height > 0.05:
+        return image
+    # get a rectangle and then a box around the largest countour
+    rect = cv2.minAreaRect(cnt)
+
+    # Draw the box
+    box = cv2.boxPoints(rect)
+    box = np.int0(box)
+    cv2.drawContours(image, [box], 0, (0, 0, 255), 2)
+    (xy, wh, rotation_angle) = (rect[0], rect[1], rect[2])
+    # Converting the width and height variables to inbetween -1 and 1
+    try:
+        (x, y) = xy
+        (w, h) = wh
+    except ValueError:
+        return image
+    if rotation_angle < -45.0 or rotation_angle > 45.0:
+        w, h = h, w
+    # Draw the centre point
+    cv2.circle(image, (int(x), int(y)), 2, (0, 0, 255), 2)
+    x = ((2 * x) / width) - 1
+    y = ((2 * y) / height) - 1
+    w = w / width
+    h = h / height
+    # Send the results to NetworkTables
+    # TODO!!
+
+    return image
+
+def init_filter():
+    return findTarget
+
+def setup_capture(device):
+    if not os.path.exists(device):
+        raise Exception("No such video device: %s" % device)
+
+    # Try to find the settings from the mjpg-streamer config file
+    v4l2_str = "v4l2-ctl -d %s" % device
+    with open('mjpg-streamer') as f:
+        for line in f:
+            if "--device %s" % device in line:
+                # A bunch of regexes to find the parameters
+                p = re.compile('-ex ([0-9]+)')
+                if p.search(line):
+                    v4l2_str += " -c exposure_auto=1 -c exposure_absolute=%s" % p.search(line).group(1)
+                    print("Found exposure setting: %s" % p.search(line).group(1))
+                p = re.compile('-br ([0-9]+)')
+                if p.search(line):
+                    v4l2_str += " -c brightness=%s" % p.search(line).group(1)
+                    print("Found brightness setting: %s" % p.search(line).group(1))
+                p = re.compile('-co ([0-9]+)')
+                if p.search(line):
+                    v4l2_str += " -c contrast=%s" % p.search(line).group(1)
+                    print("Found contrast setting: %s" % p.search(line).group(1))
+                p = re.compile('-sa ([0-9]+)')
+                if p.search(line):
+                    v4l2_str += " -c saturation=%s" % p.search(line).group(1)
+                    print("Found saturation setting: %s" % p.search(line).group(1))
+    os.system(v4l2_str)
+    cap = cv2.VideoCapture(device)
+    return cap
+
+# Allow easy capturing of sample images using same settings as on robot
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Capture sample image.')
+    parser.add_argument('--device', help='capture device', default='/dev/video0')
+    parser.add_argument('--video', help='display a live video feed of the capture', action='store_true')
+    parser.add_argument('--file', help='capture image to a file', type=str, default=None)
+    parser.add_argument('--verbose',
+            help='print the values coming back from the findTarget function for a specific file', type=str,
+            default=None)
+    parser.add_argument('--showfile', help='display a specific file with a bounding box drawn around it',
+            type=str, default=None)
+    args = parser.parse_args()
+    cap = None
+    if args.file or args.video:
+        cap = setup_capture(args.device)
+        print("Brightness: %f" % cap.get(cv2.CAP_PROP_BRIGHTNESS))
+        print("Contrast: %f" % cap.get(cv2.CAP_PROP_CONTRAST))
+        print("Saturation: %f" % cap.get(cv2.CAP_PROP_SATURATION))
+        print("Exposure: %f" % cap.get(cv2.CAP_PROP_EXPOSURE))
+    if args.file:
+        retval, image = cap.read()
+        if retval:
+            image = findTarget(None, image)
+            cv2.imwrite(args.file, image)
+    if args.verbose:
+        image = cv2.imread(args.verbose, cv2.IMREAD_COLOR)
+        image = findTarget(image)
+        # print("x: %f\ny: %f\nwidth: %f\nheight: %f" % (x, y, w, h))
+    if args.showfile:
+        image = cv2.imread(args.showfile, cv2.IMREAD_COLOR)
+        image = findTarget(image)
+        cv2.imshow('image', image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    if args.video:
+        window = cv2.namedWindow("preview")
+        while True:
+            retval, image = cap.read()
+            img = findTarget(image)
+            cv2.imshow("preview", image)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
