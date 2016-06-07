beb40bed1513c303c136321b5a0e23dcf34021fa
Arthur Allshire <arthur.allshire@gmail.com>
2016-06-08 08:07:29 +1000

    Test for boulder automation intake

--- tests/test_boulder_automation.py
+++ tests/test_boulder_automation.py
@@ -17,7 +17,43 @@ class StepController(object):
         '''Called when a new robot DS packet is received'''
         self.step += 1
         return self._on_step(tm, self.step)
-    
+
+def test_intake_boulder(control):
+    ba = BoulderAutomation()
+    ba.intake = MagicMock()
+    ba.shooter = MagicMock()
+    ba.intake.up_to_speed = MagicMock(return_value=True)
+    ba.intake.ball_detected = MagicMock(return_value=True)
+    ba.intake.slowing = MagicMock(return_value=True)
+    ba.intake.pinned = MagicMock(return_value=True)
+    setup_tunables(ba, "boulder_automation")
+    def _on_step(tm, step):
+        if step == 1:
+            ba.intake_boulder()
+            assert ba.current_state == "pre_intake"
+        elif step == 2:
+            assert ba.intake.intake.called
+            assert ba.current_state == "intaking"
+        elif step == 3:
+            pass
+        elif step <= 19:
+            assert ba.shooter.backdrive.called
+            assert ba.current_state == "intaking_contact"
+        elif step == 20:
+            assert ba.current_state == "pinning"
+        elif step == 21:
+            assert ba.shooter.backdrive.called
+            assert ba.intake.backdrive_pin.called
+            assert ba.current_state == "pinned"
+        elif step == 22:
+            assert not ba.current_state
+        else:
+            return False
+        ba.execute()  # Magicbot normally does this for us
+        return True
+    c = StepController(control, _on_step)
+    control.run_test(c)
+    assert c.step == 23
 
 def test_shoot_boulder(control):
     ba = BoulderAutomation()
