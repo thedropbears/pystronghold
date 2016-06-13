ae576563f820bf61e1597ee8b23e440ad8ef76ec
James Ward <james@thedropbears.org.au>
2016-06-13 23:33:45 +1000

    Fixed execute() method of RangeFinder class not firing.

--- robot.py
+++ robot.py
@@ -19,6 +19,7 @@ import logging
 import math
 
 class StrongholdRobot(magicbot.MagicRobot):
+    range_finder = RangeFinder
     chassis = Chassis
     intake = Intake
     shooter = Shooter
@@ -38,24 +39,23 @@ class StrongholdRobot(magicbot.MagicRobot):
         # needs to be created here so we can pass it in to the PIDController
         self.bno055 = BNO055()
         self.vision = Vision()
-        self.range_finder = RangeFinder(0)
         self.heading_hold_pid_output = BlankPIDOutput()
         Tu = 1.6
         Ku = 0.6
         Kp = Ku * 0.3
         self.heading_hold_pid = wpilib.PIDController(0.8,
                                                      0.0,
-                                                     1.5, #2.0 * Kp / Tu * 0.1, 1.0 * Kp * Tu / 20.0 * 0,
+                                                     1.5,  # 2.0 * Kp / Tu * 0.1, 1.0 * Kp * Tu / 20.0 * 0,
                                                      self.bno055, self.heading_hold_pid_output)
         """self.heading_hold_pid = wpilib.PIDController(0.6,
                                                      2.0 * Kp / Tu * 0.1,
                                                      1.0 * Kp * Tu / 20.0 * 0,
                                                      self.bno055, self.heading_hold_pid_output)"""
-        self.heading_hold_pid.setAbsoluteTolerance(3.0*math.pi/180.0)
+        self.heading_hold_pid.setAbsoluteTolerance(3.0 * math.pi / 180.0)
         self.heading_hold_pid.setContinuous(True)
         self.heading_hold_pid.setInputRange(-math.pi, math.pi)
         self.heading_hold_pid.setOutputRange(-0.2, 0.2)
-        #self.heading_hold_pid.setOutputRange(-1.0, 1.0)
+        # self.heading_hold_pid.setOutputRange(-1.0, 1.0)
         self.intake_motor.setFeedbackDevice(wpilib.CANTalon.FeedbackDevice.QuadEncoder)
         self.intake_motor.reverseSensor(False)
         self.joystick_rate = 0.3
@@ -75,7 +75,7 @@ class StrongholdRobot(magicbot.MagicRobot):
         self.sd.putDouble("raw_yaw", self.bno055.getRawHeading())
         self.sd.putDouble("raw_pitch", self.bno055.getPitch())
         self.sd.putDouble("raw_roll", self.bno055.getRoll())
-        self.sd.putDouble("shooter_speed", -self.shooter.shooter_motor.getSetpoint()) # minus sign here so +ve is shooting
+        self.sd.putDouble("shooter_speed", -self.shooter.shooter_motor.getSetpoint())  # minus sign here so +ve is shooting
         self.sd.putDouble("heading_pid_output", self.heading_hold_pid_output.output)
         self.sd.putDouble("heading_hold_pid_setpoint", self.heading_hold_pid.getSetpoint())
         self.sd.putString("boulder_state", self.boulder_automation.current_state)
@@ -84,7 +84,7 @@ class StrongholdRobot(magicbot.MagicRobot):
         self.sd.putBoolean("track_vision", self.chassis.track_vision)
         self.sd.putDouble("pov", self.joystick.getPOV())
         self.sd.putDouble("gyro_z_rate", self.bno055.getHeadingRate())
-        self.sd.putDouble("heading_hold_error", self.heading_hold_pid.getSetpoint()-self.bno055.getAngle())
+        self.sd.putDouble("heading_hold_error", self.heading_hold_pid.getSetpoint() - self.bno055.getAngle())
         self.sd.putDouble("defeater_current", self.defeater_motor.getOutputCurrent())
         self.sd.putDouble("defeater_speed", self.defeater_motor.get())
         self.sd.putDouble("joystick_throttle", self.joystick.getThrottle())
@@ -94,7 +94,7 @@ class StrongholdRobot(magicbot.MagicRobot):
         for module in self.chassis._modules.values():
             distances.append(abs(module.distance) / module.drive_counts_per_metre)
         for key, distance in zip(self.chassis._modules.keys(), distances):
-            self.sd.putDouble("encoder_motor_"+key, distance)
+            self.sd.putDouble("encoder_motor_" + key, distance)
 
     def disabledInit(self):
         self.boulder_automation.done()
@@ -211,10 +211,10 @@ class StrongholdRobot(magicbot.MagicRobot):
                     direction = math.pi
                 elif self.joystick.getPOV() == 90:
                     # shooter right goal
-                    direction = math.pi/3.0+math.pi
+                    direction = math.pi / 3.0 + math.pi
                 elif self.joystick.getPOV() == 270:
                     # shooter left goal
-                    direction = -math.pi/3.0+math.pi
+                    direction = -math.pi / 3.0 + math.pi
                 elif self.joystick.getPOV() == 180:
                     direction = 0.0
                 self.chassis.set_heading_setpoint(direction)
@@ -270,7 +270,7 @@ class StrongholdRobot(magicbot.MagicRobot):
                 self.chassis.distance_pid.disable()
                 self.chassis.range_setpoint = None
                 self.chassis.track_vision = False
-                #self.chassis.field_oriented = True
+                # self.chassis.field_oriented = True
         self.putData()
 
 
@@ -278,7 +278,7 @@ class StrongholdRobot(magicbot.MagicRobot):
         """This function is called periodically during test mode."""
         wpilib.LiveWindow.run()
 
-    def debounce(self, button, gamepad = False):
+    def debounce(self, button, gamepad=False):
         device = None
         if gamepad:
             pressed_buttons = self.pressed_buttons_gp
@@ -308,11 +308,11 @@ def rescale_js(value, deadzone=0.0, exponential=0.0, rate=1.0):
     if abs(value) < deadzone:
         return 0.0
     elif exponential == 0.0:
-        value = (value-deadzone)/(1-deadzone)
+        value = (value - deadzone) / (1 - deadzone)
     else:
-        a = math.log(exponential+1)/(1-deadzone)
-        value = (math.exp(a*(value - deadzone))-1)/exponential
-    return value*value_negative*rate
+        a = math.log(exponential + 1) / (1 - deadzone)
+        value = (math.exp(a * (value - deadzone)) - 1) / exponential
+    return value * value_negative * rate
 
 
 if __name__ == "__main__":
